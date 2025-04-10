e3a5b9acfccfc7b66b0752bf13582d6e0db41632
Artur Dias <arturdrm@hotmail.com>
2025-04-10 10:37:24 -0300

    Modificado intervalos de pertinência

--- fuzzy-api/app/models/fuzzy_model.py
+++ fuzzy-api/app/models/fuzzy_model.py
@@ -8,15 +8,15 @@ def setup_fuzzy_model():
     abertura_valvula = ctrl.Consequent(np.arange(0, 101, 1), 'abertura_valvula')
 
     # Funções de pertinência para o erro
-    erro['zero'] = fuzz.trimf(erro.universe, [0, 0, 25])
-    erro['positivo'] = fuzz.trimf(erro.universe, [0, 25, 50])
-    erro['positivo_grande'] = fuzz.trimf(erro.universe, [25, 50, 75])
-    erro['positivo_muito_grande'] = fuzz.trimf(erro.universe, [50, 100, 100])
+    erro['zero'] = fuzz.trimf(erro.universe, [-3, 0, 0])
+    erro['positivo'] = fuzz.trimf(erro.universe, [10, 30, 60])
+    erro['positivo_grande'] = fuzz.trimf(erro.universe, [50, 65, 80])
+    erro['positivo_muito_grande'] = fuzz.trimf(erro.universe, [70, 100, 100])
 
     # Funções de pertinência para a abertura da válvula
-    abertura_valvula['fechada'] = fuzz.trimf(abertura_valvula.universe, [0, 0, 50])
-    abertura_valvula['parcial'] = fuzz.trimf(abertura_valvula.universe, [0, 50, 75])
-    abertura_valvula['quase_aberta'] = fuzz.trimf(abertura_valvula.universe, [50, 75, 100])
+    abertura_valvula['fechada'] = fuzz.trimf(abertura_valvula.universe, [0, 0, 0])
+    abertura_valvula['parcial'] = fuzz.trimf(abertura_valvula.universe, [25, 35, 50])
+    abertura_valvula['quase_aberta'] = fuzz.trimf(abertura_valvula.universe, [50, 65, 75])
     abertura_valvula['aberta'] = fuzz.trimf(abertura_valvula.universe, [75, 100, 100])
 
     # Regras fuzzy
