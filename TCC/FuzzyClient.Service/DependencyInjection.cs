60b134b7ec97a700c3913e379d231f90996c219c
Artur Dias <artur.vicente@auvo.com.br>
2025-05-02 09:26:16 -0300

    Configuração dos serviços da api

--- TCC/FuzzyClient.Service/DependencyInjection.cs
+++ TCC/FuzzyClient.Service/DependencyInjection.cs
@@ -5,6 +5,26 @@ namespace FuzzyClient.Service;
 
 public static class DependencyInjection
 {
+    private static IServiceCollection ConfigureServicesInternal(
+        this IServiceCollection services, 
+        ApiSettings apiSettings)
+    {
+        return services
+            .Configure<ApiSettings>(opt =>
+            {
+                opt.Port = apiSettings.Port;
+                opt.DebugMode = apiSettings.DebugMode;
+            })
+            .Configure<ApiOptions>(opt => opt.EndPoint = $"http://localhost:{apiSettings.Port}/api/");
+    }
+
+    private static IServiceCollection AddApiServicesInternal(this IServiceCollection services)
+    {
+        return services            
+            .AddTransient<IApiService, ApiService>()
+            .AddHostedService<ApiHostedService>();
+    }
+
     public static IServiceCollection AddApiServices(this IServiceCollection services)
     {
         var apiSettings = new ApiSettings
@@ -14,13 +34,19 @@ public static class DependencyInjection
         };
         
         return services
-            .Configure<ApiSettings>(opt =>
-            {
-                opt.Port = apiSettings.Port;
-                opt.DebugMode = apiSettings.DebugMode;
-            })
-            .Configure<ApiOptions>(opt => opt.EndPoint = $"http://localhost:{apiSettings.Port}/api/")
-            .AddTransient<IApiService, ApiService>()
-            .AddHostedService<ApiHostedService>();
+            .ConfigureServicesInternal(apiSettings)
+            .AddApiServicesInternal();
+    }
+
+    public static IServiceCollection AddApiServices(
+        this IServiceCollection services, 
+        Action<ApiSettings> configure)
+    {
+        ApiSettings apiSettings = new();
+        configure(apiSettings);
+
+        return services
+            .ConfigureServicesInternal(apiSettings)
+            .AddApiServicesInternal();
     }
 }
