26faf22631da366ba9bf3b2d8085ed76a8c0a765
Artur Dias <arturdrm@hotmail.com>
2025-05-18 17:58:26 -0300

    Removed unused projects

--- TCC/FuzzyClient.Console/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using FuzzyClient.Service;
-using Microsoft.Extensions.Hosting;
-
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddApiServices(builder.Configuration);
-
-var host = builder.Build();
-
-await host.RunAsync();
