fda30d9d92817fc7c0429a5a824d65f2dbc30a2e
Artur Dias <arturdrm@hotmail.com>
2025-05-21 11:19:03 -0300

    Save and read settings

--- TCC/TCC.UI/TCC.UI.RazorLib/Shared/Settings.razor.cs
+++ TCC/TCC.UI/TCC.UI.RazorLib/Shared/Settings.razor.cs
@@ -1,7 +1,22 @@
 using Microsoft.AspNetCore.Components;
+using TCC.Shared.Models;
+using TCC.Shared.Services;
 
 namespace TCC.UI.RazorLib.Shared;
 
 public partial class Settings : ComponentBase
 {
+    private SettingsModel _settings = new();
+
+    private async Task SaveAsync()
+    {
+        await SettingsService.SaveSettingsAsync(_settings);
+    }
+    
+    [Inject] private ISettingsService SettingsService { get; set; } = null!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _settings = await SettingsService.GetSettingsAsync();
+    }
 }
