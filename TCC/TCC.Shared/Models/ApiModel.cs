807aac14cae5a03c69a9755519bc07d82bf487c9
Artur Dias <arturdrm@hotmail.com>
2025-08-12 10:35:55 -0300

    Adicionada configuração de tolerância

--- TCC/TCC.Shared/Models/ApiModel.cs
+++ TCC/TCC.Shared/Models/ApiModel.cs
@@ -3,4 +3,5 @@ namespace TCC.Shared.Models;
 public class ApiModel
 {
     public string Endpoint { get; set; } = "http://localhost:40123/api/";
+    public double Tolerance { get; set; } = 0.02;
 }
